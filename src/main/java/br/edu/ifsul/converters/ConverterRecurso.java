5fdb7e7dac03982a99285d510cde03e9d8af4317
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-06-14 07:01:42 -0300

    .

---
+++ src/main/java/br/edu/ifsul/converters/ConverterRecurso.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.converters;
+
+import br.edu.ifsul.condominiomodel.Recurso;
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author darla
+ */
+@Named(value = "converterRecurso")
+@RequestScoped
+public class ConverterRecurso implements Serializable, Converter {
+
+    @PersistenceContext(unitName = "Condominio-PWPU")
+    protected EntityManager em;    
+    
+    // converte da tela para o objeto
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+        if (string == null || string.equals("Selecione um registro")){
+            return null;
+        }
+        return em.find(Recurso.class, Integer.parseInt(string));
+    }
+
+    // converte do objeto para a tela
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, Object t) {
+        if (t == null){
+            return null;
+        }
+        Recurso obj = (Recurso) t;
+        return obj.getId().toString();
+    }
+
+}
