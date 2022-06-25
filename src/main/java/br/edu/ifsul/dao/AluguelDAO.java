42b65719c0edf022e5d12e27c80de7507c41b4ac
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-06-25 08:41:30 -0300

    subrelatorios

--- src/main/java/br/edu/ifsul/dao/AluguelDAO.java
+++ src/main/java/br/edu/ifsul/dao/AluguelDAO.java
@@ -5,6 +5,7 @@
 package br.edu.ifsul.dao;
 
 import br.edu.ifsul.condominiomodel.Aluguel;
+import br.edu.ifsul.condominiomodel.Mensalidades;
 import br.edu.ifsul.converters.ConverterOrdem;
 import java.io.Serializable;
 import java.util.List;
@@ -32,10 +33,10 @@ public class AluguelDAO <TIPO> extends DAOGenerico<Aluguel> implements Serializa
         Aluguel obj = em.find(Aluguel.class, id);
         obj.getMensalidades().size();
         return obj;
-    }    
+    }
     
-    public List<Aluguel> getListaObjetosCompleta(){
-        String jpql = "select distinct t from aluguel t order by t.id";
+    public List<Mensalidades> getListaObjetosCompleta(){
+        String jpql = "from Aluguel t left join fetch t.mensalidades order by t.id";
         return em.createQuery(jpql).getResultList();
     }
 }
